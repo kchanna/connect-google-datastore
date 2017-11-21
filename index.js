ff106338e6cc89ef849ede77aff827ba8b125726
keshav <keshavbc@outlook.com>
2017-11-21 20:46:43 +0530

    Refering to lib/connect*.js file

---
+++ index.js
@@ -0,0 +1,9 @@
+/*!
+ * Connect - Google Data Store
+ * Copyright(c) 2017 Keshav Channa
+ * MIT Licensed
+
+ https://github.com/kchanna/connect-google-datastore
+ */
+
+module.exports = require('./lib/connect-google-datastore');
